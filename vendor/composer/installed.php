0ac83e8c3c309a0d7f22c4e92ba1b01f0dc48cd5
Marcelo Assis Santos <marceloassissantos@MacBook-Pro-de-Marcelo.local>
2022-11-30 16:41:56 -0300

    composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.1',
+        'version' => '1.0.1.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'laudirbispo/imobi',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'laudirbispo/imobi' => array(
+            'pretty_version' => '1.0.1',
+            'version' => '1.0.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
