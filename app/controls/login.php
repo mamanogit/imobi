71a7771206184a947b4d83d51f11dfc0c171788e
Marcelo Assis Santos <marceloassissantos@MacBook-Pro-de-Marcelo.local>
2022-12-01 00:11:17 -0300

    fix heading json only

--- app/controls/login.php
+++ app/controls/login.php
@@ -3,7 +3,7 @@ header('Content-Type: application/json');
 require_once ($_SERVER['DOCUMENT_ROOT'].'/config/autoload.php');
 require_once ($_SERVER['DOCUMENT_ROOT'].'/config/config.php');
 require_once ($_SERVER['DOCUMENT_ROOT'].'/app/controls/adminFunctions.php');
-//require_once ($_SERVER['DOCUMENT_ROOT'].'/config/public_functions.php');
+require_once ($_SERVER['DOCUMENT_ROOT'].'/config/public_functions.php');
 
 use app\controls\authUser;
 use app\controls\session;
